3566a8ed3f3b859ad1a191550197f6ad1fad935c
Saumya Majumder <mailme@isaumya.com>
2013-07-07 02:53:57 +0530

    GPL License link change

--- zip.php
+++ zip.php
@@ -7,7 +7,7 @@
  * http://www8.isaumya.com
  *
  * Licensed under the GPL license v3:
- * http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * http://www.gnu.org/licenses/gpl.html
  */
 	
 		$directory = base64_decode($_REQUEST['dr']);
